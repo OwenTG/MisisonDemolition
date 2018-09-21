8bf9a829fbe2c69e4983aca9671a99e57c2da2c0
OwenTG <42875734+OwenTG@users.noreply.github.com>
2018-09-21 16:24:42 -0400

    Projectile Fixed - Delete Library

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour
+{ 
+    //fields set in the Unity Inspector pane
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    //fields set dynamically
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    
+    {
+        // print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        //The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        //Instantiate a Projectile
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+        //Start it at the launchpoint
+        projectile.transform.position = launchPos;
+        //Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
